b1c7af9a8cff495af66f4f7ad14b1c286e237260
Yogi <yodeput@gmail.com>
2026-06-25 11:34:34 +0700

    chore: scaffold core library modules

---
+++ core/autoclick/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.kunnn.totap.core.autoclick"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 29
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
